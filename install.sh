dd3f788f66f12e26e1649c333081f354c9143702
pelle.wielinga <pelle.wielinga@gmail.com>
2017-06-23 18:52:49 +0200

    Add vimperatorrc

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s "$DOTFILES_DIR/vim/.vimrc" "$HOME/.vimrc"
-ln -s "$DOTFILES_DIR/vim/.vim" "$HOME/.vim"
+function safelink {
+  FILE=$1
+  LINK=$2
+
+  echo "$LINK -> $FILE"
+  if [ ! -e "$LINK" ]; then
+    ln -s "$FILE" "$LINK"
+  fi
+}
+
+safelink "$DOTFILES_DIR/vim/.vimrc" "$HOME/.vimrc"
+safelink "$DOTFILES_DIR/vim/.vim" "$HOME/.vim"
+safelink "$DOTFILES_DIR/vim/.vimperatorrc" "$HOME/.vimperatorrc"
+
 
